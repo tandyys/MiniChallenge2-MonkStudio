c7ea05cc1a3a853900a427bd63e406cfb6eb144a
Tandy Sabana <tansabana12@gmail.com>
2024-06-18 12:12:26 +0700

    Initial Commit

---
+++ LevelOneMini2/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  LevelOneMini2
+//
+//  Created by tandyys on 18/06/24.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
